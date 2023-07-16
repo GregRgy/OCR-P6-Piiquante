733f561c06858199314e63ca9739461afa14acff
GregRgy <gregrgy12@outlook.com>
2023-07-16 15:19:57 +0200

    Ajout de multer pour authentification, des routes et controllers, à venir : likes/dislikes et sécurisation propriétaire de la sauce

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,43 @@
 const express = require('express');
-const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const productRoutes = require('./routes/product');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+
+const saucesRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+mongoose.connect("mongodb+srv://GregRgy:gpawn@cluster01.3ia81ri.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true,
+     useUnifiedTopology: true 
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
-
-mongoose.connect(
-    "mongodb+srv://GregRgy:gpawn@cluster01.3ia81ri.mongodb.net/?retryWrites=true&w=majority", //Add your connection string from MongoDB
-    { useNewUrlParser: true, 
-      useUnifiedTopology: true })
-      .then(() => console.log('Connection à MongoDB réussie'))
-      .catch(() => console.log('Connection à MongoDB échouée'));
-
-app.use((req, res, next) => {
+app.use((req, res, next) => 
+{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("Requête reçue !");
-    next();
-});
+app.use(bodyParser.json()); 
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+app.use(mongoSanitize()); 
+app.use(helmet()); 
 
-app.use((req, res, next) => {
-    res.json({message: "Votre requête a bien été reçue !"});
-    next();
-});
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
+
+app.use('/images', express.static(path.join(__dirname, 'images'))); 
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+    
 
 module.exports = app;
---
+++ backend/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user');
+
+//routes POST pour créer un compte ou se connecter envoyé par le frontend
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+//export du router
+module.exports = router;
